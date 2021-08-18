d5238bc2851a5b84bb5c2e8746e4d01da7aacddc
Prince Ahmed <codingwithprince@gmail.com>
2021-08-18 18:17:01 +0600

    about section added

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
-// const bigText = document.getElementById('bigText');
-// const menu = document.getElementById('menu');
-//         menu.addEventListener('click', ()=>{
-//             if(bigText.style.marginTop != '550px'){
-//                 bigText.style.marginTop = '550px';
-//                 bigText.style.transition = '0.5s';
-//             }
-//             else{
-//                 bigText.style.marginTop = '0';
-//                 bigText.style.transition = '0.5s';
-//             }
-//         });
+// sticky nav
+document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+          document.getElementById('navbar_top').classList.add('fixed-top');
+          // add padding top to show content behind navbar
+          navbar_height = document.querySelector('.navbar').offsetHeight;
+          document.body.style.paddingTop = navbar_height + 'px';
+          document.querySelector('.navbar').style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+        } else {
+          document.getElementById('navbar_top').classList.remove('fixed-top');
+           // remove padding top from body
+          document.body.style.paddingTop = '0';
+          document.querySelector('.navbar').style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+        } 
+    });
+  }); 
+
 AOS.init({
     duration: 1200,
+});
+
+// read more handler
+const readMore = document.getElementById('readMore');
+const hide = document.getElementById('hide');
+readMore.addEventListener('click', ()=>{
+    if(hide.style.display != 'block'){
+        hide.style.display = "block";
+        readMore.innerText = "Read less";
+    }else{
+        hide.style.display = "none";
+        readMore.innerText = "Read more";
+    }
+    
 });
