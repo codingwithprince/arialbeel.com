1be17dbfaf467c98d59d56278700adb9b7f091dd
Prince Ahmed <codingwithprince@gmail.com>
2021-08-18 11:48:05 +0600

    added bg

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// const bigText = document.getElementById('bigText');
+// const menu = document.getElementById('menu');
+//         menu.addEventListener('click', ()=>{
+//             if(bigText.style.marginTop != '550px'){
+//                 bigText.style.marginTop = '550px';
+//                 bigText.style.transition = '0.5s';
+//             }
+//             else{
+//                 bigText.style.marginTop = '0';
+//                 bigText.style.transition = '0.5s';
+//             }
+//         });
